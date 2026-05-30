0923703c70a14c6411485259957b2dce03cb5d24
irinapodlisnyuk <irinapodlisnyuk@gmail.com>
2026-05-30 14:37:50 +0300

    server

--- my-app/server.js
+++ my-app/server.js
@@ -21,28 +21,13 @@ if (process.env.NODE_ENV !== "production") {
   );
 }
 
-// API маршруты
+
 app.use("/api", routes);
 
 // Раздача статических файлов (фронтенд после npm run build теперь лежит в dist)
 app.use(express.static(path.join(__dirname, "dist")));
 app.use("/audio", express.static(path.join(__dirname, "src/audio")));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
-});
-
-// Явный маршрут для страницы профиля/музыки
-app.get("/main", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "main.html"));
-});
-
-// Если вы в коде переходите на 'main.html' с расширением, то добавьте и этот вариант:
-app.get("/main.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "main.html"));
-});
-
-// ИСПРАВЛЕНО: Берем порт от Render или 8000 для локалки
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
